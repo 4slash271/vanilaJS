b207f6077845cc12f072bd400a93e43acec8517a
4slash271 <4slash271@gmail.com>
2021-09-27 22:21:01 +0900

    using toggle instead contains

--- js/3-7.js
+++ js/3-7.js
@@ -1,6 +1,6 @@
 const h1 = document.querySelector("h1")
 
-function handleTitleClick(){
+/*function handleTitleClick(){
   const clikcked = "active"
 
     if(h1.classList.contains(clikcked)){ //classLIst는 class를 목록으로 사용할 수 있게 해줌
@@ -8,6 +8,13 @@ function handleTitleClick(){
     }
     else{h1.classList.add(clikcked);}
   
-}
+}*/
+
+function handleTitleClick(){
+ 
+
+  h1.classList.toggle("active"); //toggle이 위의 기능을 한줄로 처리할 수 있게 해줌
+  
+}  //단 CSS의 클래스를 그대로 가져와야 한다. (변수 선언 안했기 때문에)
 
 h1.addEventListener("click",handleTitleClick);
