bfeffb31416d06a423c62fd40b6cad35a47e783a
4slash271 <4slash271@gmail.com>
2021-09-28 21:43:38 +0900

    4.2 using HTML form and delete conditional

--- js/4-0.js
+++ js/4-0.js
@@ -7,18 +7,23 @@ const ligonButton = loginForm.querySelector("button");
 
 /*위의 코드를 요약해서 바꾸면!*/ 
 
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input"); 
+// const loginButton = document.querySelector("#login-form button");
 
-function loginBtnClick(){
+function onLoginSubmit(){
+    // const username = loginInput.value;
+    // if(username === ""){
+    //   alert("Please write your name");
+    // }
+    // else if(username.length > 15) {
+    //     alert("Your name is too long");
+
+    // }
+
     const username = loginInput.value;
-    if(username === ""){
-      alert("Please write your name");
-    }
-    else if(username.length > 15) {
-        alert("Your name is too long");
-
-    }
+    console.log(username);
 }
 
-loginButton.addEventListener("click",loginBtnClick);
+// loginButton.addEventListener("click",onLoginSubmit);
+loginForm.addEventListener("submit",onLoginSubmit); //submit은 엔터키나 버튼을 누를 때 발생한다
