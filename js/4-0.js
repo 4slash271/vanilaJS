d94bd209394de331c378c645302d78dce6ca5ec7
4slash271 <4slash271@gmail.com>
2021-09-29 03:37:19 +0900

    add username on greeting using innerText and combine variable and strings

--- js/4-0.js
+++ js/4-0.js
@@ -9,6 +9,9 @@ const ligonButton = loginForm.querySelector("button");
 
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input"); 
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 // const loginButton = document.querySelector("#login-form button");
 
 
@@ -27,16 +30,18 @@ function onLoginSubmit(event){
     event.preventDefault();//이벤트의 전파를 막지 않고 이벤트 취소. submit 하되 새로고침 되지 않고 이벤트 사라진다.
     const username = loginInput.value;//username은 login 창의 input
     loginForm.classList.add("hidden");//loginForm에 hidden 클래스 추가 {display=none}
-    console.log(username);
+    // greeting.innerText = "Hello " + username + "!"; 아래와 같은 스트링과 변수 같이 쓰는 법
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
     //onLoginSubmit 함수가 실행되면 이벤트의 취소되고 로그인 폼 사라짐 
 }
-function handleLinkClick(){
-    alert("clicked!!")
-}//handleLickClick 실행하면 "clicked!!"라는 스트링의 문구 출력
+// function handleLinkClick(){
+//     alert("clicked!!")
+// }//handleLickClick 실행하면 "clicked!!"라는 스트링의 문구 출력
 
 
 // loginButton.addEventListener("click",onLoginSubmit);
 loginForm.addEventListener("submit",onLoginSubmit); //submit은 엔터키나 버튼을 누를 때 발생한다
 //loginForm을 submit하면 onLoginSubmit 함수가 실행된다.
 
-link.addEventListener("click", handleLinkClick); //링크를 click하면 handleLinkClick 함수 실행
+// link.addEventListener("click", handleLinkClick); //링크를 click하면 handleLinkClick 함수 실행
