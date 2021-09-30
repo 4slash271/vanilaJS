767f8bea51b1d900c31a04312502a7c1809b2ef2
4slash271 <4slash271@gmail.com>
2021-09-30 23:41:43 +0900

    making random number game..

---
+++ js/challenge03.js
@@ -0,0 +1,11 @@
+const rangeForm = document.querySelector("#range-form");
+const rangeInput = document.querySelector("#range-form input");
+const chosenNumberForm = document.querySelector("#chosen-number-form");
+const chosenNumberInput = document.querySelector("#chosen-number-form .chosed-blank");
+
+
+
+
+function chosenNumberSubmit(){
+    const chosenNumber = chosenNumberInput.value
+}
