dbfd3ec7f465923e00efd592ca56ee6a060e5c3d
4slash271 <4slash271@gmail.com>
2021-10-02 23:55:20 +0900

    6.0 start randome foods recommendation

---
+++ js/foods_recommended.js
@@ -0,0 +1,37 @@
+const foods = [
+    {
+        food: "후라이드 치킨",
+        restaurant: "굽네",
+    },
+    {
+        food: "피자",
+        restaurant: "빨간모자",
+    },
+    {
+        food: "평양냉면",
+        restaurant: "더평양",
+    },
+    {
+        food: "초밥",
+        restaurant: "스시쇼부",
+    },
+    {
+        food: "짬뽕",
+        restaurant: "유경장",
+    },
+    {
+        food: "닭한마리",
+        restaurant: "공릉동 닭한마리",
+    },
+    {
+        food: "햄버거",
+        restaurant: "바스버거",
+    },
+    
+    
+]
+
+const food = document.querySelector("#foods span:first-child");
+const restaurant = document.querySelector("#foods span:last-child");
+
+console.log(foods[3]);
