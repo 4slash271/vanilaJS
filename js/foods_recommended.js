bc1dd91f395bf79a105a7985dc13613273c68f53
4slash271 <4slash271@gmail.com>
2021-10-03 00:19:51 +0900

    6.0 made foods recommendation randomly whit Math.random/floor/length

--- js/foods_recommended.js
+++ js/foods_recommended.js
@@ -33,8 +33,21 @@ const foods = [
 
 const food = document.querySelector("#foods span:first-child");
 const restaurant = document.querySelector("#foods span:last-child");
+const randomFoods = foods[Math.floor(Math.random()*foods.length)];
 
-console.log(foods[3]);
+food.innerText = randomFoods.food;
+restaurant.innerText = randomFoods.restaurant;
 
+/*
 console.log(Math.random()*10);
-//randome은 무작위의 수를 보여주지만 소수점 아래는 제거되지 않음
+//random은 무작위의 수를 보여주지만 소수점 아래는 제거되지 않음
+console.log(Math.round(2.999)); //반올림
+console.log(Math.floor(29.9)); //내림
+console.log(Math.ceil(2.1)); //올림
+*/
+
+//round나 ceil을 쓰면 7 이상의 소수를 8로 받아 undefined 뜰 수 있음
+/*
+console.log(foods[Math.floor(Math.random()*7)]); //이렇게 작성하면 인덱스에 새로운 항목이 추가될 경우 랜덤에 반영하기 힘들다!
+console.log(foods[Math.floor(Math.random()*foods.length)]); 
+*/
